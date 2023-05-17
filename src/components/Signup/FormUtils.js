9a74eb463ba8530e81e361822d9ea63d29bf253c
Kashyap Patel <kashyap.p@simformsolutions.com>
2023-05-17 17:30:27 +0530

    Add user-profile validation

--- src/components/Signup/FormUtils.js
+++ src/components/Signup/FormUtils.js
@@ -15,6 +15,18 @@ export const validationSchema = Yup.object().shape({
     phoneNo: Yup.string().length(10,'Phone no is have to be 10 character').matches(/^[0-9]{10}$/,'Only digits are allowed').required('Phone no is required'),
     password:Yup.string().required('password is required'),
     confirmPassword:Yup.string().oneOf([Yup.ref('password'),null],'Password must match').required('Confirm Password is required'),
+    profilePhoto:Yup.mixed().test('fileType', 'Invalid file format', (value) => {
+        if (!value) {
+          return true // No file selected, validation passes
+        }
+        const supportedFormats = ['image/jpeg', 'image/png', 'image/gif'];
+        return supportedFormats.includes(value.type)
+      }).test('fileType',"Image size should be less than 2MB",(value)=>{
+        if (!value) {
+            return true // No file selected, validation passes
+        }
+        return value.size<=2*1024*1024; // 2MB
+      }).required('Profile photo is required')
 })
 
 export const submitHandler = (values)=>{
